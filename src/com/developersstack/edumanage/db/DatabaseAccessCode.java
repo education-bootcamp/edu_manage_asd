94180846b42d77bb29c82ffb54ab4a1ed8acbc81
ozen-hub <info.ozenhub@gmail.com>
2023-03-26 21:02:13 +0530

    2

--- src/com/developersstack/edumanage/db/DatabaseAccessCode.java
+++ src/com/developersstack/edumanage/db/DatabaseAccessCode.java
@@ -1,7 +1,22 @@
 package com.developersstack.edumanage.db;
 
+import com.developersstack.edumanage.model.User;
+import com.developersstack.edumanage.util.security.PasswordManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DatabaseAccessCode {
     // user manage ===============>
-
+    public boolean saveUser(User user) throws SQLException , ClassNotFoundException{
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement stm = connection.prepareStatement("INSERT INTO user VALUES(?,?,?,?)");
+        stm.setString(1, user.getEmail());
+        stm.setString(2, user.getFirstName());
+        stm.setString(3, user.getLastName());
+        stm.setString(4, new PasswordManager().encrypt(user.getPassword()));
+        return stm.executeUpdate()>0;
+    }
     // user manage ===============>
 }
