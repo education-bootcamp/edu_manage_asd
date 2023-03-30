c570cbdfd7be65ee3103ada6eb9b849f45540def
ozen-hub <info.ozenhub@gmail.com>
2023-03-30 21:58:41 +0530

    18

---
+++ src/com/developersstack/edumanage/bo/custom/StudentBo.java
@@ -0,0 +1,18 @@
+package com.developersstack.edumanage.bo.custom;
+
+import com.developersstack.edumanage.dto.StudentDto;
+import com.developersstack.edumanage.entity.Student;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface StudentBo {
+    public boolean saveStudent(StudentDto dto) throws SQLException, ClassNotFoundException;
+    public boolean updateStudent(StudentDto t) throws SQLException, ClassNotFoundException;
+    public boolean deleteStudent(String id) throws SQLException, ClassNotFoundException;
+    public StudentDto findStudent(String id) throws SQLException, ClassNotFoundException;
+    public ArrayList<StudentDto> findAllStudents();
+    public String findStudentLastId() throws SQLException, ClassNotFoundException;
+
+    public ArrayList<StudentDto> searchStudents(String searchText) throws SQLException, ClassNotFoundException;
+}
--- src/com/developersstack/edumanage/repo/custom/impl/UserRepoImpl.java
+++ src/com/developersstack/edumanage/repo/custom/impl/UserRepoImpl.java
@@ -1,12 +1,10 @@
 package com.developersstack.edumanage.repo.custom.impl;
 
-import com.developersstack.edumanage.db.DbConnection;
+
 import com.developersstack.edumanage.entity.User;
 import com.developersstack.edumanage.repo.CrudUtil;
 import com.developersstack.edumanage.repo.custom.UserRepo;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
