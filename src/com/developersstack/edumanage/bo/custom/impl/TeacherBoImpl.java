82c41323378ab80075ce89faa127aa71e3188a22
ozen-hub <info.ozenhub@gmail.com>
2023-03-30 22:42:04 +0530

    20

--- src/com/developersstack/edumanage/bo/custom/impl/TeacherBoImpl.java
+++ src/com/developersstack/edumanage/bo/custom/impl/TeacherBoImpl.java
@@ -2,38 +2,64 @@ package com.developersstack.edumanage.bo.custom.impl;
 
 import com.developersstack.edumanage.bo.custom.TeacherBo;
 import com.developersstack.edumanage.dto.TeacherDto;
+import com.developersstack.edumanage.entity.Teacher;
+import com.developersstack.edumanage.repo.custom.TeacherRepo;
+import com.developersstack.edumanage.repo.custom.impl.TeacherRepoImpl;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class TeacherBoImpl implements TeacherBo {
+
+    private final TeacherRepo teacherRepo= new TeacherRepoImpl();
+
     @Override
     public boolean saveTeacher(TeacherDto dto) throws SQLException, ClassNotFoundException {
-        return false;
+        return teacherRepo.save(
+                new Teacher(dto.getCode(),dto.getName(),
+                        dto.getAddress(),dto.getContact())
+        );
     }
 
     @Override
     public boolean updateTeacher(TeacherDto dto) throws SQLException, ClassNotFoundException {
-        return false;
+        return teacherRepo.update(
+                new Teacher(dto.getCode(),dto.getName(),
+                        dto.getAddress(),dto.getContact())
+        );
     }
 
     @Override
     public boolean deleteTeacher(String id) throws SQLException, ClassNotFoundException {
-        return false;
+        return teacherRepo.delete(id);
     }
 
     @Override
     public TeacherDto findTeacher(String id) throws SQLException, ClassNotFoundException {
+        Teacher teacher = teacherRepo.find(id);
+        if (null!=teacher){
+            return new TeacherDto(
+                    teacher.getCode(),teacher.getName(),teacher.getAddress(),teacher.getContact()
+            );
+        }
         return null;
     }
 
     @Override
     public String findTeacherLastId() throws SQLException, ClassNotFoundException {
-        return null;
+        return teacherRepo.findTeacherLastId();
     }
 
     @Override
     public ArrayList<TeacherDto> searchTeachers(String searchText) throws SQLException, ClassNotFoundException {
-        return null;
+        ArrayList<Teacher> list = teacherRepo.findAllTeachers(searchText);
+        ArrayList<TeacherDto> dtos = new ArrayList<>();
+        for (Teacher t:list
+             ) {
+            dtos.add(new TeacherDto(
+                    t.getCode(),t.getName(),t.getAddress(),t.getContact()
+            ));
+        }
+        return dtos;
     }
 }
--- src/com/developersstack/edumanage/bo/custom/impl/UserBoImpl.java
+++ src/com/developersstack/edumanage/bo/custom/impl/UserBoImpl.java
@@ -2,17 +2,34 @@ package com.developersstack.edumanage.bo.custom.impl;
 
 import com.developersstack.edumanage.bo.custom.UserBo;
 import com.developersstack.edumanage.dto.UserDto;
+import com.developersstack.edumanage.entity.User;
+import com.developersstack.edumanage.repo.custom.UserRepo;
+import com.developersstack.edumanage.repo.custom.impl.UserRepoImpl;
+import com.developersstack.edumanage.util.security.PasswordManager;
 
 import java.sql.SQLException;
 
 public class UserBoImpl implements UserBo {
+
+
+    private final UserRepo userRepo = new UserRepoImpl();
+    private final PasswordManager passwordManager = new PasswordManager();
     @Override
     public boolean saveUser(UserDto dto) throws SQLException, ClassNotFoundException {
-        return false;
+        return userRepo.save(
+                new User(dto.getFirstName(),dto.getLastName(),
+                        dto.getEmail(),
+                        passwordManager.encrypt(dto.getPassword()))
+        );
     }
 
     @Override
     public UserDto findUser(String id) throws SQLException, ClassNotFoundException {
+        User user = userRepo.find(id);
+        if (null!=user){
+            return new UserDto(user.getFirstName(), user.getLastName(),
+                    user.getEmail(), user.getPassword());
+        }
         return null;
     }
 }
